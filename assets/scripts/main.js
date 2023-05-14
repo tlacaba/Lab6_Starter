7c400b4b4c4d4e7a8c6d2cfdcc33ef529b7a4e3d
tlacaba <timlacaba123@gmail.com>
2023-05-14 16:46:15 -0700

    Completed Expose

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -24,6 +24,8 @@ function getRecipesFromStorage() {
   // A9. TODO - Complete the functionality as described in this function
   //           header. It is possible in only a single line, but should
   //           be no more than a few lines.
+  return JSON.parse(localStorage.getItem('recipes'));
+  // familarize yourself with JSON documentation and stuff
 }
 
 /**
@@ -35,10 +37,19 @@ function getRecipesFromStorage() {
  */
 function addRecipesToDocument(recipes) {
   // A10. TODO - Get a reference to the <main> element
+  let mainEl = document.querySelector('main');
+
   // A11. TODO - Loop through each of the recipes in the passed in array,
   //            create a <recipe-card> element for each one, and populate
   //            each <recipe-card> with that recipe data using element.data = ...
   //            Append each element to <main>
+
+  recipes.forEach(function(recipe) {
+    let newRecipeCard = document.createElement('recipe-card');
+    newRecipeCard.data = recipe;
+    mainEl.appendChild(newRecipeCard);
+  });
+  // familarize yourself with forEach notation, and take note that we already set up the .data functionality in RecipeCard.js to allow use to easily do this in two lines.
 }
 
 /**
